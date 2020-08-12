6ea08abba5f3294b6a9446475a095254ec590e10
test <test@tests-MacBook-Pro.local>
2020-08-12 16:06:25 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,8 @@
+a = 40.5
+b = 20
+a = 100.5 * b
+print ("sum of a and b   ", a + b)
+print ("product of a and b   ", a * b)
+print ("subtraction with a and b   ", a - b)
+print ("quotient of a and b   " , a / b)
+
